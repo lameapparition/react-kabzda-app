44e84d8bd7271345b84626217ab0b7627badf15f
Yegor Platonov <ep291100@gmail.com>
2022-06-28 23:36:56 +0300

    Соцсеть v1

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src='https://www.ideal.nl/img/logo/ideal-logo-1024.png' />
+        </header>
+    );
+}
+
+export default Header;
